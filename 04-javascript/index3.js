35dd728a0f5975f83f447bab7dc4514fa72d1652
ZZOOONII <gks3380@gamil.com>
2023-07-26 14:55:33 +0900

    study: js 자료형, 형변환

--- 04-javascript/index3.js
+++ 04-javascript/index3.js
@@ -1,7 +1,7 @@
-console.log('connected!')
+console.log("connected!");
 
 // 변수 선언: 변수 정의
-// 변수 할당: = 연산자를 이용해 값을 넣음 
+// 변수 할당: = 연산자를 이용해 값을 넣음
 // 초기화: 변수에 처음 할당하는 순간
 
 // ### var
@@ -50,7 +50,6 @@ const c = 3;
 console.log(c);
 // c = 33; // error: const 변수는 재할당 불가능!
 
-
 // 변수 이름 규칙 (=> 식별자 규칙)
 // 식별자: 이름을 붙일 때 사용하는 단어 (변수명, 함수명, 클래스명, ....)
 // - 키워드(예약어) 사용 불가능
@@ -75,16 +74,16 @@ console.log(c);
 // 1. string (문자열)
 // : 텍스트 정보, 따옴표 감싸야 함
 // : 숫자, 특수문자도 따옴표 안에 포함? 문자열
-let text = '안녕하세요!';
-let text2 = '하이!';
+let text = "안녕하세요!";
+let text2 = "하이!";
 let gender = "남자";
 console.log(text);
 console.log(text2);
 console.log(text + text2);
-console.log(text + '저는 Sean 입니다. 저의 성별은' + gender + '입니다!');
+console.log(text + "저는 Sean 입니다. 저의 성별은" + gender + "입니다!");
 console.log(text, text2, "ㅋㅋ");
 // 템플릿 리터럴 (es6) 백틱(`)와 달러, 중괄호(${}) 조합으로 사용
-// : 문자열 내에서 변수와, 상수를 간결히 표현 
+// : 문자열 내에서 변수와, 상수를 간결히 표현
 console.log(`${text} 저는 Sean 입니다. 저의 성별은 ${gender}입니다!`);
 
 // 2. number (숫자형)
@@ -100,7 +99,7 @@ console.log(num * 2);
 console.log(num / 2);
 
 // 3. boolean (불리언)
-// true, false 
+// true, false
 let isWater = true;
 let hasApple = false;
 console.log(isWater, hasApple);
@@ -112,22 +111,157 @@ console.log(temp);
 temp = "Hello~";
 console.log(temp);
 
-// 5. undefined 
+// 5. undefined
 // 값도 없고, 타입도 지정되지 않은 상태
 let x; // 변수 선언만 했을 때 초기 값으로 undefined 할당
 console.log(x); // undefined
 
-
 // [QUIZ] 변수
 // Q1. 코드 실행시 q1 변수는 어떤 값이 될까?
-let q1 = 3;
-q1 - 2;
+// let q1 = 3;
+// q1 - 2;
+// console.log(x);
 
-// Q2. 다음 코드 실행시 q2 변수는 어떤 값이 될까?
-let q2 = 10;
-q2 = q2 + 5;
-q2 = q2 - 2;
+// // Q2. 다음 코드 실행시 q2 변수는 어떤 값이 될까?
+// let q2 = 10;
+// q2 = q2 + 5;
+// q2 = q2 - 2;
+// console.log(x);
 
-// Q3. 다음 코드 실행시 q3 변수는 어떤 값이 될까?
-const q3 = 1;
-q3 = q3 + 1;
+// // Q3. 다음 코드 실행시 q3 변수는 어떤 값이 될까?
+// const q3 = 1;
+// q3 = q3 + 1;
+// console.log(x);
+
+// 6. array (배열)
+// 배열의 이름: fruits
+// 배열의 원소(아이템): 배열 안에 있는 데이터 하나하나
+// 배열의 위치(index): 0부터 시작
+// 배열의 길이(크기): 원소의 개수와 동일
+const fruits = ["orange", "pineapple", "grape", "apple"];
+// const fruits2 = new Array('orange', 'pineapple', 'grape', 'apple');
+console.log(fruits);
+console.log(fruits[0]); // fruits 배열의 위치가 0인 원소 출력
+
+// js에서는 배열 원소의 자료형이 달라도 됨!
+const data = [1, "allie", false, null, undefined];
+console.log(data);
+
+// Array 안에 Array 들어갈 수 있음 (중첩 가능) => 2차원 배열
+const korean = [
+  ["가", "나", "다"],
+  ["라", "마", "바"],
+  ["사", "아", "자"],
+];
+console.log(korean);
+console.log(korean[0]); // ['가', '나', '다']
+console.log(korean[1]);
+console.log(korean[2]);
+
+console.log(korean[0][2]); // '다'
+
+// 퀴즈 korean 배열에서 '가자' 글씨 출력하기
+console.log(korean[0][0] + korean[2][2]);
+
+// 퀴즈 3차원 배열에서 숫자 8 출력
+const nums = [
+  [
+    [1, 2, 3],
+    [4, 5, 6],
+  ],
+  [
+    [7, 8, 9],
+    [10, 11, 12],
+  ],
+];
+console.log(nums[1][0][1]);
+
+// 7. object (key: value)
+const cat = {
+  name: "나비",
+  age: 1,
+  isCute: true,
+  mew: function () {
+    return "냐옹";
+  },
+};
+console.log(cat); // object 자체를 출력
+
+// object 의 속성(key)에 접근하는 방법
+// (1) 점 표기법 (.)
+console.log(cat.name);
+console.log(cat.age);
+console.log(cat.mew());
+// (2) 대괄호 표기법 ([])
+console.log(cat["name"]);
+// key 가 변수에 저장되어 있을 때 주로 사용
+const tempVal = "name";
+console.log(cat[tempVal]); // => cat['name] => cat.name 과 동일
+
+// typeof : 자료형 확인할 수 있는 키워드
+// - typeof : 자료형 확인할 수 있는 키워드
+// - typeof(x)
+console.log(typeof "문자"); // string
+console.log(typeof 12345); // number
+console.log(typeof 3.14); // number
+console.log(typeof true); // boolean
+console.log(typeof false); // boolean
+console.log(typeof null); // object ** js 공식적으로 인정한 언어 자체의 오류
+console.log(typeof undefined); // undefined
+console.log(typeof fruits); // object
+console.log(typeof cat); // object
+
+const me = {
+  name: "jiyoon",
+  age: 28,
+  isLeader: false,
+  job: "student",
+};
+console.log(me);
+
+// 형변환
+// 성적 평균 구하는 프로그램 만들기
+// let mathScore = prompt('수학점수 입력');
+// console.log(mathScore, typeof mathScore);
+// let engScore = prompt('영어점수 입력');
+// console.log(engScore, typeof engScore);
+// let avg = (mathScore + engScore) / 2;
+// console.log(`수학과 영어의 평균 점수는 ${avg}점 입니다~ `);
+
+// 1. String(): 문자로 형변환
+let str1 = true;
+let str2 = 123;
+let str3 = null;
+
+console.log(String(str1), typeof String(str1));
+console.log(String(str2), typeof String(str2));
+console.log(String(str3), typeof String(str3));
+console.log(str1.toString(), typeof str1.toString());
+
+// 2. Number(): 숫자로 형변환
+let n1 = true;
+let n2 = false;
+let n3 = "123.9";
+
+console.log(Number(n1), typeof Number(n1)); // 숫자로 바꾸면 1
+console.log(Number(n2), typeof Number(n2)); // 숫자로 바꾸면 0
+console.log(Number(n3), typeof Number(n3)); // 123.9
+console.log(parseInt(n3, 10)); // n3 값을 10진수의 정수(int)로 바꾸겠다 => 123
+console.log(parseFloat(n3)); // n3 값을 실수(float)로 바꾸겠다 => 123.9
+
+// 형변환 실습
+// let mathScore = prompt("수학 점수를 입력");
+// let engScore = prompt("영어 점수를 입력");
+// let avgScore = (Number(mathScore) + Number(engScore)) / 2;
+// console.log(`수학과 영어 점수의 평균은 ${avgScore} 입니다.~`);
+
+// 형변환 다른 사람들꺼
+let mathScore = Number("77");
+let engScore = Number("88");
+let avgScore = (mathScore + engScore) / 2;
+console.log(avgScore);
+
+// +) null, underfined를 숫자로 바꾼다면?
+console.log(Number(undefined), typeof Number(undefined)); // NaN, number
+// Not-A-Number(숫자가 아님)
+console.log(Number(null), typeof Number(undefined)); // 0, number
