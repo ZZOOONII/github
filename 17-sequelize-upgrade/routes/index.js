f19a34edbe85569cab130b872451b6e98ff4c6b0
ZZOOONII <gks3380@gmail.com>
2023-09-01 17:13:35 +0900

    study: sequelize association

--- 17-sequelize-upgrade/routes/index.js
+++ 17-sequelize-upgrade/routes/index.js
@@ -1,8 +1,37 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const controller = require('../controller/Cmain');
+const controller = require("../controller/Cmain");
 
 // TODO: 라우터 정의
+// localhost:PORT/ 기본 주소
 
+router.get("/", controller.index);
+
+// ~~~~~~~~~~~~ 선수관련 API ~~~~~~~~~~~~
+
+// get /players - 전체 선수 조회
+router.get("/players", controller.getPlayers);
+
+// get /players/:player_id  - 특정 선수 조회
+router.get("/players/:player_id", controller.getPlayer);
+
+// post /players - 선수 추가
+router.post("/players", controller.postPlayer);
+
+// patch /players/:player_id/team - 특정 선수의 소속 팀 변경
+router.patch("/players/:player_id/team", controller.patchPlayer);
+
+// delete /players/:player_id - 특정 선수 삭제
+router.delete("/players/:player_id", controller.deletePlayer);
+
+// ~~~~~~~~~ 팀 관련 API ~~~~~~~~~
+// get /teams - 전체 팀 조회
+router.get("/teams", controller.getTeams);
+
+// get /teams/:team_id - 특정 팀 조회
+router.get("/teams/:team_id", controller.getTeam);
+
+// get /teams/:team_id/players - 특정 팀의 모든 선수 조회
+router.get("/teams/:team_id/players", controller.getTeamPlayers);
 
 module.exports = router;
