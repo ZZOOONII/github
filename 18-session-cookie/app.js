c29b21dad8ef67ac7cf89dabc356e8a6c3bede9c
ZZOOONII <gks3380@gmail.com>
2023-09-04 13:57:47 +0900

    study: session

--- 18-session-cookie/app.js
+++ 18-session-cookie/app.js
@@ -9,6 +9,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // TODO: cookie parser 미들웨어 등록
+app.use(cookieParser());
+const mycookieConf = {
+  httpOnly: true,
+  maxAge: 20 * 1000,
+  signed: true,
+};
 
 app.get("/", (req, res) => {
   // *다음과 같이 기능 구현하였는데, 굳이 이렇게 하지 않아도 됩니다.
@@ -17,12 +23,21 @@ app.get("/", (req, res) => {
   // console.log('req.cookies.popup >> ', req.cookies.popup);
 
   // TODO: index.ejs render할 때 두 번째 인자로 popup key 로 요청의 쿠키값 보내기
+
+  app.get("/setCookie", (req, res) => {
+    // res.cookie(쿠키 이름, 쿠키 값, 쿠키 옵션)
+    res.cookie("myCookie", "myValue", mycookieConf);
+    res.send("Cookie 설정 완료!");
+  });
+
+  const popup = req.cookies.popup;
   res.render("index", {});
 });
 
 app.post("/setcookie", (req, res) => {
   // TODO: 쿠키 생성
   // 쿠키 이름: 'popup', 쿠키 값: 'hide'
+  res.cookie("popup", "hide", mycookieConf);
   res.send("쿠키 설정 성공!!");
 });
 
