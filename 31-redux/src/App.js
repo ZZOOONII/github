c6abb0b58f6794b97e3985f381b0b95af37441f9
ZZOOONII <gks3380@gmail.com>
2023-10-16 18:58:05 +0900

    study: props drilling 체험

--- 31-redux/src/App.js
+++ 31-redux/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [number, setNumber] = useState(100);
+  const plus = () => setNumber(number + 1);
+  const minus = () => setNumber(number - 1);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Redux Ex</h1>
+      <Box1 number={number} plus={plus} minus={minus} />
     </div>
   );
 }
+
+const Box1 = ({ number, plus, minus }) => {
+  return (
+    <div className="Box">
+      <h2>Box2: {number}</h2>
+      <Box2 number={number} plus={plus} minus={minus} />
+    </div>
+  );
+};
+
+const Box2 = ({ number, plus, minus }) => {
+  return (
+    <div className="Box">
+      <h2>Box3: {number}</h2>
+      <Box3 number={number} plus={plus} minus={minus} />
+    </div>
+  );
+};
+
+const Box3 = ({ number, plus, minus }) => {
+  return (
+    <div className="Box">
+      <h2>Box4: {number}</h2>
+      <Box4 number={number} plus={plus} minus={minus} />
+    </div>
+  );
+};
 
 export default App;
